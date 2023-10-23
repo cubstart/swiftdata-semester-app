b743290508b70c7bdaea57a01b0034d1d8c5c691
justinwongeecs <jwonghelloworld@berkeley.edu>
2023-10-23 14:34:21 -0700

    Initial Commit

---
+++ SwiftDataLec7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataLec7
+//
+//  Created by Justin Wong on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataLec7/SwiftDataLec7App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataLec7App.swift
+//  SwiftDataLec7
+//
+//  Created by Justin Wong on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataLec7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
